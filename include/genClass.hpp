cc140afa4bfe93678247b784488be55922cbecd8
Genraim <egarmin95@gmail.com>
2015-04-25 14:30:36 +0500

    Написал структуру для хранения информации о точке, пару мелких функций и заголовочный файл

---
+++ include/genClass.hpp
@@ -0,0 +1,4 @@
+#ifndef GEN_CLASS_H_
+#define GEN_CLASS_H_
+struct vec;
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,31 @@
 //Authors: Genraim and Quilch
 #include <iostream>
+#include "genClass.hpp"
+
+using namespace std;
+
+int read(vec);
+
 int main() {
-	std::cout << "Hello world\n";
+	vec constvec; //здесь будем хранить коэф при каждой неизвестной, а также правую часть уравения (свободный член)
+	constvec.type = 0;
+	read(constvec);
+
+}
+int read(vec &cvec) {
+	cout << "Программа для нахождения корней линейного уравнения с 4-я неизвестными \n" <<
+			"--------------------------------" << "Введите коэффициенты: " << endl;
+
+	cout << "a: \n";
+	cin >> cvec.variable[0];
+	cout << "\n b: \n";
+	cin >> cvec.variable[1];
+	cout << "\n c: \n";
+	cin >> cvec.variable[2];
+	cout << "\n d: \n";
+	cin >> cvec.variable[3];
+	cout << "\n Свободный член: \n";
+	cin >> cvec.solution;
 	return 0;
 }
+
